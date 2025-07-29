486f703009640d6986e96f04aa423f0904414248
Liiiiilith <simonamoldovanni@gmail.com>
2025-07-29 13:25:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function mergiLaPagina2() {
+    window.location.href = 'pagina2.html';
+}
+
+function mergiLaPagina3() {
+    window.location.href = 'pagina3.html';
+}
+
+function mergiLaPagina4() {
+    window.location.href = 'pagina4.html';
+}
